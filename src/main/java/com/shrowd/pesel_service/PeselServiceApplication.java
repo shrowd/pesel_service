d8680ad9ffbd9ada53331b6faddd6f608638f0d7
shrowd <118308030+shrowd@users.noreply.github.com>
2023-08-09 16:03:33 +0200

    initial commit

---
+++ src/main/java/com/shrowd/pesel_service/PeselServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shrowd.pesel_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeselServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeselServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shrowd/pesel_service/PeselServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrowd.pesel_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeselServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
